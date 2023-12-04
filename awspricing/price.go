03798335621bbae0e3b504c9d111fb743de66744
mrhello369 <admin@kubefin.dev>
2023-12-05 00:06:20 +0800

    feat: add awspricing lib for query ec2 price
    Signed-off-by: mrhello369 <admin@kubefin.dev>

---
+++ awspricing/price.go
@@ -0,0 +1,247 @@
+/*
+Copyright 2023 The KubeFin Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package awspricing
+
+import (
+	"embed"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/fs"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"k8s.io/klog/v2"
+)
+
+//go:embed data/*.json
+var file embed.FS
+
+type AWSEC2PriceClient struct {
+	latestPriceDataTime time.Time
+	priceData           *GeneralPriceData
+	priceDataURL        string
+}
+
+func NewAWSEC2PriceClient(region string) (*AWSEC2PriceClient, error) {
+	if region == "" {
+		return nil, fmt.Errorf("parameter region is needed")
+	}
+
+	client := &AWSEC2PriceClient{
+		priceDataURL: GlobalPriceDataBaseURL + region + "/index.json",
+		priceData:    &GeneralPriceData{},
+	}
+
+	if _, ok := ReginList[region]; !ok {
+		return nil, fmt.Errorf("unknown region: %s", region)
+	}
+	lastUpdatedTime, err := time.Parse(time.RFC1123, ReginList[region])
+	if err != nil {
+		return nil, err
+	}
+	client.latestPriceDataTime = lastUpdatedTime
+
+	if strings.HasPrefix(region, "cn-") {
+		client.priceDataURL = CNPriceDataBaseURL + region + "/index.json"
+	}
+
+	data, err := fs.ReadFile(file, "data/"+region+".json")
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, client.priceData)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func (a *AWSEC2PriceClient) Start() {
+	for range time.Tick(time.Second) {
+		a.updatePriceData()
+	}
+}
+
+func (a *AWSEC2PriceClient) updatePriceData() {
+	latestUpdateTime, err := getPriceDataLatestUpdateTime(a.priceDataURL)
+	if err != nil {
+		klog.Errorf("get latest price data time failed: %v", err)
+		return
+	}
+	if err == nil && !latestUpdateTime.Equal(a.latestPriceDataTime) {
+		klog.Infof("EC2 price data updated, start downloading the latest data....")
+		resp, err := http.Get(a.priceDataURL)
+		if err != nil {
+			klog.Errorf("get price data failed: %v", err)
+			return
+		}
+		klog.Infof("EC2 price data downloading finished....")
+		defer resp.Body.Close()
+
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			klog.Errorf("read price data failed: %v", err)
+			return
+		}
+		awsData, err := ParseData(data)
+		if err != nil {
+			klog.Errorf("parse price data failed: %v", err)
+			return
+		}
+		generalData, err := ConvertToGeneralPriceData(awsData)
+		if err != nil {
+			klog.Errorf("convert price data failed: %v", err)
+			return
+		}
+		a.priceData = generalData
+		a.latestPriceDataTime = latestUpdateTime
+	}
+}
+
+func (a *AWSEC2PriceClient) GetOnDemandEC2PriceInfo(instanceType string) (*EC2GeneralPrice, error) {
+	if instanceType == "" {
+		return nil, fmt.Errorf("invalid input, parameter instanceType is needed")
+	}
+
+	for _, item := range *a.priceData {
+		//return &item, nil
+		if item.InstanceType == instanceType {
+			return &item, nil
+		}
+	}
+
+	return nil, fmt.Errorf("no matched price info found, instanceType: %s", instanceType)
+}
+
+func (a *AWSEC2PriceClient) ListEC2PriceInfo() []EC2GeneralPrice {
+	ret := []EC2GeneralPrice{}
+	ret = append(ret, *a.priceData...)
+
+	return ret
+}
+
+func getPriceDataLatestUpdateTime(priceDataUrl string) (time.Time, error) {
+	req, err := http.NewRequest("HEAD", priceDataUrl, nil)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return time.Time{}, err
+	}
+	defer resp.Body.Close()
+
+	t := resp.Header.Get("Last-Modified")
+	if t == "" {
+		return time.Time{}, fmt.Errorf("no last modified time")
+	}
+
+	ret, err := time.Parse(time.RFC1123, t)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	return ret, nil
+}
+
+func ParseData(data []byte) (*AWSEC2Data, error) {
+	ec2Data := &AWSEC2Data{}
+	if err := json.Unmarshal(data, ec2Data); err != nil {
+		return nil, err
+	}
+
+	return ec2Data, nil
+}
+
+func ConvertToGeneralPriceData(data *AWSEC2Data) (*GeneralPriceData, error) {
+	generalData := GeneralPriceData{}
+	for id, resourceItem := range data.Products {
+		if resourceItem.Attributes.InstanceType == "" {
+			continue
+		}
+
+		// In most scenarios, there is shared VM
+		if resourceItem.Attributes.Tenancy != Shared {
+			continue
+		}
+		// We only care about Linux VM price
+		if resourceItem.Attributes.OperatingSystem != "Linux" {
+			continue
+		}
+
+		// This means it installed other sf
+		if resourceItem.Attributes.InstanceSKU != "" {
+			continue
+		}
+
+		// Following code is referring to: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/billing-info-fields.html
+		// We only care about standard Linux
+		if resourceItem.Attributes.Operation != "RunInstances" {
+			continue
+		}
+
+		// Following code is referring to: https://docs.aws.amazon.com/cost-management/latest/userguide/ce-filtering.html
+		usageType := strings.Split(resourceItem.Attributes.UsageType, "-")
+		if len(usageType) < 2 {
+			continue
+		}
+		usageTypeWithOutInstanceType := strings.Split(usageType[1], ":")
+		if len(usageTypeWithOutInstanceType) < 2 || (usageTypeWithOutInstanceType[0] != "BoxUsage" && usageTypeWithOutInstanceType[0] != "Reservation") {
+			continue
+		}
+
+		cpu, err := strconv.ParseFloat(resourceItem.Attributes.VCPU, 64)
+		if err != nil {
+			cpu = 0.0
+		}
+		memory, err := extractMemory(resourceItem.Attributes.Memory)
+		if err != nil {
+			memory = 0.0
+		}
+		item := EC2GeneralPrice{
+			InstanceType: resourceItem.Attributes.InstanceType,
+			Region:       resourceItem.Attributes.RegionCode,
+			VCPU:         cpu,
+			Memory:       memory,
+		}
+
+		item.PriceModel = PriceModelOnDemand
+		for key, priceItem := range data.Terms.OnDemand[id] {
+			if !strings.HasPrefix(key, id) {
+				continue
+			}
+			for _, priceDimensions := range priceItem.PriceDimensions {
+				item.PriceUnit = priceDimensions.Unit
+				item.PricePerUnit = priceDimensions.PricePerUnit
+				generalData = append(generalData, item)
+			}
+		}
+	}
+
+	return &generalData, nil
+}
+
+func extractMemory(memory string) (float64, error) {
+	s := strings.TrimSuffix(memory, " GiB")
+	return strconv.ParseFloat(s, 64)
+}
---
+++ awspricing/types.go
@@ -0,0 +1,186 @@
+/*
+Copyright 2023 The KubeFin Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package awspricing
+
+const (
+	CNPriceDataBaseURL     = "https://pricing.cn-north-1.amazonaws.com.cn/offers/v1.0/cn/AmazonEC2/current/"
+	GlobalPriceDataBaseURL = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/"
+)
+
+var ReginList = map[string]string{
+	"ap-southeast-1-bkk-1":    "Fri, 17 Nov 2023 17:44:44 GMT",
+	"af-south-1-los-1":        "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-north-1-hel-1":        "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-sea-1":         "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-west-2-las-1":         "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-east-1-dfw-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-central-2":            "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-south-1":              "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-phl-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-msp1":      "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-west-2":               "Fri, 17 Nov 2023 17:44:46 GMT",
+	"ap-southeast-3":          "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-west-3":               "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-south-1-ccu-1":        "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-wl1-sea1":      "Fri, 17 Nov 2023 17:44:46 GMT",
+	"eu-west-2-wl1-man1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-iah1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-northeast-2":          "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-phx-1":         "Fri, 17 Nov 2023 17:44:46 GMT",
+	"ap-northeast-1":          "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-msp-1 ":        "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-southeast-2-per-1":    "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-northeast-1-tpe-1":    "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-chi-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-central-1-wl1-ber1":   "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-gov-east-1":           "Fri, 17 Nov 2023 17:44:45 GMT",
+	"eu-west-2":               "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-pdx-1":         "Fri, 17 Nov 2023 17:44:46 GMT",
+	"us-east-1-wl1-nyc1":      "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-east-1-atl-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-dfw1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-central-1-wl1-dtm1":   "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-west-2-wl1-lon1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-gov-west-1":           "Fri, 17 Nov 2023 17:44:45 GMT",
+	"ca-central-1-wl1-yto1":   "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-southeast-2-akl-1":    "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-central-1-waw-1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-atl1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-northeast-2-wl1-sel1": "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-wl1-phx1":      "Fri, 17 Nov 2023 17:44:48 GMT",
+	"us-west-2-wl1-las1":      "Fri, 17 Nov 2023 17:44:45 GMT",
+	"eu-west-1":               "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-north-1":              "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-southeast-4":          "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-northeast-2-wl1-cjj1": "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-was1":      "Fri, 17 Nov 2023 17:44:45 GMT",
+	"eu-central-1-ham-1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-south-2":              "Fri, 17 Nov 2023 17:44:47 GMT",
+	"ap-northeast-3":          "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-wl1-den1":      "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-west-2-lax-1":         "Fri, 17 Nov 2023 17:44:45 GMT",
+	"ap-northeast-1-wl1-kix1": "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-north-1-cph-1":        "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-mia-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-northeast-1-wl1-nrt1": "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-2":               "Fri, 17 Nov 2023 17:44:45 GMT",
+	"me-south-1":              "Fri, 17 Nov 2023 17:44:44 GMT",
+	"il-central-1":            "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-bue-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-bos-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-nyc-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-clt1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"sa-east-1":               "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-east-1":               "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1":               "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-east-1-wl1":           "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-west-2-wl1-lax1":      "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-east-1-qro-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-den-1":         "Fri, 17 Nov 2023 17:44:45 GMT",
+	"us-east-1-wl1-tpa1":      "Fri, 17 Nov 2023 17:44:45 GMT",
+	"ap-south-1-del-1":        "Fri, 17 Nov 2023 17:44:44 GMT",
+	"eu-central-1":            "Fri, 17 Nov 2023 17:44:44 GMT",
+	"me-central-1":            "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-lim-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-scl-1":         "Fri, 17 Nov 2023 17:44:45 GMT",
+	"me-south-1-mct-1":        "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-bna1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-iah-1":         "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-chi1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"cn-northwest-1":          "Sat, 18 Nov 2023 01:05:01 GMT",
+	"ap-southeast-1":          "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-mci-1":         "Fri, 17 Nov 2023 17:44:45 GMT",
+	"eu-central-1-wl1-muc1":   "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-mia1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-south-1":              "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-1":               "Fri, 17 Nov 2023 17:44:45 GMT",
+	"ap-south-2":              "Fri, 17 Nov 2023 17:44:44 GMT",
+	"af-south-1":              "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-west-2-wl1":           "Fri, 17 Nov 2023 17:44:46 GMT",
+	"cn-north-1":              "Sat, 18 Nov 2023 01:05:01 GMT",
+	"ca-central-1":            "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-southeast-2":          "Fri, 17 Nov 2023 17:44:44 GMT",
+	"ap-southeast-1-mnl-1":    "Fri, 17 Nov 2023 17:44:44 GMT",
+	"us-east-1-wl1-dtw1":      "Fri, 17 Nov 2023 17:44:44 GMT",
+}
+
+type AWSEC2Data struct {
+	OfferCode string             `json:"offerCode"`
+	Products  map[string]Product `json:"products"`
+	Terms     Term               `json:"terms"`
+}
+
+type Product struct {
+	Attributes Attributes `json:"attributes"`
+}
+
+type Attributes struct {
+	VCPU            string      `json:"vcpu"`
+	Memory          string      `json:"memory"`
+	InstanceType    string      `json:"instanceType"`
+	RegionCode      string      `json:"regionCode"`
+	Tenancy         TenancyType `json:"tenancy"`
+	UsageType       string      `json:"usagetype"`
+	InstanceSKU     string      `json:"instancesku"`
+	OperatingSystem string      `json:"operatingSystem"`
+	Operation       string      `json:"operation"`
+}
+
+type TenancyType string
+
+const (
+	Shared TenancyType = "Shared"
+)
+
+type Term struct {
+	OnDemand map[string]OfferDetail `json:"OnDemand"`
+	Reserved map[string]OfferDetail `json:"Reserved"`
+}
+
+type OfferDetail map[string]OfferTerm
+
+type OfferTerm struct {
+	PriceDimensions map[string]PriceDimension `json:"priceDimensions"`
+}
+
+type PriceDimension struct {
+	Description  string       `json:"description"`
+	Unit         string       `json:"unit"`
+	PricePerUnit PricePerUnit `json:"pricePerUnit"`
+}
+
+type PricePerUnit struct {
+	USD string `json:"USD"`
+	CNY string `json:"CNY"`
+}
+
+type GeneralPriceData []EC2GeneralPrice
+
+type EC2GeneralPrice struct {
+	InstanceType string       `json:"instanceType"`
+	Region       string       `json:"region"`
+	PriceModel   string       `json:"priceModel"`
+	VCPU         float64      `json:"vcpu"`
+	Memory       float64      `json:"memory"`
+	PriceUnit    string       `json:"unit"`
+	PricePerUnit PricePerUnit `json:"pricePerUnit"`
+}
+
+const (
+	PriceModelOnDemand string = "onDemand"
+	PriceModelReserved string = "reserved"
+)
